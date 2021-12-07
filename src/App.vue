66256d87966332f4dabd70442bf752d2383b7970
Alwin <alwinlee0915@gmail.com>
2021-12-07 14:21:35 +0800

    add totalincome component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <BicycleShop title="福克斯自行車店"/>
+  <BicycleShop title="福克斯自行車店" v-bind:maxbikes="maxbikes" @rentBike="rentBike" @retuenBike="retuenBike"/>
+  <TotalIncome title="Total Income" v-bind:rental="rental" v-bind:fee="fee" @resetBike="resetBike" />
 </template>
 
 <script>
 import BicycleShop from './components/BicycleShop.vue'
+import TotalIncome from './components/TotalIncome.vue'
 
 export default {
   name: 'App',
   components: {
-    BicycleShop
+    BicycleShop, TotalIncome
+  },
+  data() {
+    return {
+      fee: 100,
+      maxbikes: 20,
+      rental: 0
+    }
+  },
+  methods: {
+    resetBike(e) {
+      this.maxbikes = e
+    },
+    rentBike(e) {
+      this.rental += e
+      //alert(this.rental)
+    },
+    retuenBike() {
+
+    },
   }
 }
 </script>
